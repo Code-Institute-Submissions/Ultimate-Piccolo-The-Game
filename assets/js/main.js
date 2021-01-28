c311943bfa3f5568c337e36a46696649a4c410cf
waterrot <oudloosdrecht@gmail.com>
2021-01-28 23:21:45 +0000

    made the buttons in modal show the price

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(".btn1beer").on("click", function(){
+    $(".amountbeer").text("2 euro");
+});
+
+$(".btn6beer").on("click", function(){
+    $(".amountbeer").text("5 euro");
+});
+
+$(".btn24beer").on("click", function(){
+    $(".amountbeer").text("10 euro");
+});
