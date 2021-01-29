a80a49fc3dfe7930d94147282390aed07e19b62d
waterrot <oudloosdrecht@gmail.com>
2021-01-29 00:47:07 +0000

    made the paypal link responsive with JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
-//function to show the price of 1 beer
+//function to show the price of 1 beer and change paypal link
 $(".btn1beer").on("click", function(){
     $(".amountbeer").text("2 euro");
+    document.getElementById("btn-link").href = "https://www.paypal.com/paypalme/RaymondHuisman/2";
 });
 
-//function to show the price of 6 beers
+//function to show the price of 6 beers and change paypal link
 $(".btn6beer").on("click", function(){
     $(".amountbeer").text("5 euro");
+    document.getElementById("btn-link").href = "https://www.paypal.com/paypalme/RaymondHuisman/5";
 });
 
-//function to show the price of 24 beers
+//function to show the price of 24 beers and change paypal link
 $(".btn24beer").on("click", function(){
     $(".amountbeer").text("10 euro");
+    document.getElementById("btn-link").href = "https://www.paypal.com/paypalme/RaymondHuisman/10";
 });
