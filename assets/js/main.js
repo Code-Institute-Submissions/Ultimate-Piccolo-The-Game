567c5fbc6f7e9405421cdd12689ff5bfda2ca273
waterrot <oudloosdrecht@gmail.com>
2021-01-29 00:03:56 +0000

    added comments to JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
+//function to show the price of 1 beer
 $(".btn1beer").on("click", function(){
     $(".amountbeer").text("2 euro");
 });
 
+//function to show the price of 6 beers
 $(".btn6beer").on("click", function(){
     $(".amountbeer").text("5 euro");
 });
 
+//function to show the price of 24 beers
 $(".btn24beer").on("click", function(){
     $(".amountbeer").text("10 euro");
 });
