b381ac6d80d2891df137ccdad88a59e4e6019918
waterrot <oudloosdrecht@gmail.com>
2021-02-01 16:34:53 +0000

    added function who gets usernames part1

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,26 @@ $(".btn24beer").on("click", function(){
     $(".amountbeer").text("10 euro");
     document.getElementById("btn-link").href = "https://www.paypal.com/paypalme/RaymondHuisman/10";
 });
+
+
+function getUserName() {
+var player1 = document.getElementById('player1').value;
+var result = document.getElementById('result');
+
+if (player1.length < 3) {
+    result.textContent = 'Username must contain at least 3 characters';
+    //alert('Username must contain at least 3 characters');
+} else {
+    result.textContent = 'Your username is: ' + player1;
+    //alert(nameField);
+}
+}
+
+var subButton = document.getElementById('subButton');
+subButton.addEventListener('click', getUserName, false); 
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }};
