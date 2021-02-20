09c68f47ca70d7ea6944d90383a609c0303392f0
waterrot <oudloosdrecht@gmail.com>
2021-02-20 22:34:32 +0000

    made 2 different js files

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,3 @@
-//code for the give us a beer modal
-//function to show the price of 1 beer and change paypal link
-$(".btn1beer").on("click", function () {
-    $(".amountbeer").text("2 euro");
-    document.getElementById("btn-link").href = "https://www.paypal.com/paypalme/RaymondHuisman/2";
-});
-
-//function to show the price of 6 beers and change paypal link
-$(".btn6beer").on("click", function () {
-    $(".amountbeer").text("5 euro");
-    document.getElementById("btn-link").href = "https://www.paypal.com/paypalme/RaymondHuisman/5";
-});
-
-//function to show the price of 24 beers and change paypal link
-$(".btn24beer").on("click", function () {
-    $(".amountbeer").text("10 euro");
-    document.getElementById("btn-link").href = "https://www.paypal.com/paypalme/RaymondHuisman/10";
-});
-
 // variables for the player and game page.
 var clickNextDiv = document.getElementById('startGame');    //select the whole card div
 var output = document.getElementById('gamePage');           //select the div where the game text will come in
